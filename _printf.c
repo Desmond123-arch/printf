4af54d172d1595cd1b02039c365dc93d4f9a4658
Desmends27 <desmondmends27@gmail.com>
2023-01-24 14:17:45 +0000

    added header files and neccessary files

---
+++ _printf.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int _printf(const char *format,...)
+{
+	va_list args;
+	va_start(args,format);
+	int i
---
+++ main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN__H
+#define MAIN_H
+#include "unistd.h"
+int _putchar(char c);
+int _printf(const char *format, ...);
+#endif /* MAIN_H */
