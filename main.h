f696a51090f61ff621eafec4b594ffbca9ad1b3f
Desmends27 <109918035+Desmends27@users.noreply.github.com>
2023-01-24 16:17:49 +0000

    Update main.h
    added new functions

--- main.h
+++ main.h
@@ -1,10 +1,25 @@
-#ifndef MAIN__H
-#define MAIN_H
-#include "unistd.h"
-#include "stdarg.h"
-int _putchar(char c);
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
 int _printf(const char *format, ...);
-char* convert (unsigned int, int);
-void _print_string(char *);
-
-#endif /* MAIN_H */
+int (*check_specifier(const char *format))(va_list);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int _putchar(char c);
+/**
+  *struct func - Data type structure for specifier pointers
+  * @t: character string
+  * @f: function that takes a function pointer
+  */
+typedef struct func
+{
+	char *t;
+	int (*f)(va_list);
+} func_t;
+#endif
