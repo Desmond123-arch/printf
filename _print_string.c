5bba6ecf5d99f9fd697bf4d1f054fe291f30dff8
Desmends27 <109918035+Desmends27@users.noreply.github.com>
2023-01-24 16:20:18 +0000

    Update _print_string.c
    Task 1

--- _print_string.c
+++ _print_string.c
@@ -1,14 +1,37 @@
 #include "main.h"
+
 /**
- * _print_string : prints a string
- * @s: pointer to string
+ * print_c - prints a character
+ * @c: character to print
+ * Return: Always 1
  */
-void _print_string(char *s)
+
+int print_c(va_list c)
 {
-	int i;
-	while (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-	}
+	char ch = (char)va_arg(c, int);
+
+	_putchar(ch);
+	return (1);
 }
+
+/**
+ * print_s - prints a string
+ * @s: string to be printed
+ * Return: number of characters printed
+ */
+
+int print_s(va_list s)
+{
+	int count;
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+	for (count = 0; str[count]; count++)
+	{
+		_putchar(str[count]);
+	}
+
+	return (count);
